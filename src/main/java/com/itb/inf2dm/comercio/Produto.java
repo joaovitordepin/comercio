20147051c50a47e92f9eeff949c44eb6e028a839
joaovitordepin <ceocareoficial@gmail.com>
2023-06-06 09:21:03 -0300

    Tela de produto e model produto

--- src/main/java/com/itb/inf2dm/comercio/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/Produto.java
@@ -2,4 +2,11 @@ package com.itb.inf2dm.comercio;
 
 public class Produto {
 
+	
+	private Long id;
+	private String descricao;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
 }
