b2c2c7a3517a2a3ab0d13b6de80b0a7454e791e2
joaovitordepin <ceocareoficial@gmail.com>
2023-08-01 12:13:40 -0300

    02/07/23

--- src/main/java/com/itb/inf2dm/comercio/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/Produto.java
@@ -8,9 +8,60 @@ public class Produto {
 	private String nome;
 	private String codigoBarras;
 	private double preco;
+	private boolean codStatus;
+	public long setId;
 	
 	
 	//public: modificador de acesso livre para todas as classes 
 	//private: Modificador de acesso restrito apenas para membros 
 	//da propria classe, Entende-se por "membros" (atributos e/ou metodos)
+	
+	// Métodos Setter's e Getter's : Adicionar e Recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	
+	public boolean getStatus() {
+		return codStatus;
+	}
+
+	public void setStatus(boolean codStatus) {
+		this.codStatus = codStatus;
+	}
 }
--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -1,11 +1,14 @@
 package com.itb.inf2dm.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.itb.inf2dm.comercio.Produto;
-
+import com.itb.inf2dm.comercio.Produto; 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
@@ -13,14 +16,30 @@ public class LojaController {
 	
 	List<Produto> listaDeProdutos = new ArrayList<Produto>();
 	@GetMapping("/listar")
-	public String listarProdutos() {
+	public String listarProdutos(Model model) {
 
 		Produto p1 = new Produto();
-		p1.setId (201);
+		p1.setId(20l);
 		p1.setNome("Máquina de Lavar Brastemp 12 litros");
 		p1.setCodigoBarras("JHDYEJDUSNEU25411");
-		p1.setPreco(3654.12);
-	    	
+		p1.setPreco(3654.12);	
+		p1.setDescricao("Brastempaa");
+		p1.setStatus(false);
+		
+		listaDeProdutos.add(p1);
+		
+		Produto p2 = new Produto();
+		p2.setId(13l);
+		p2.setNome("Xbox One");
+		p2.setCodigoBarras("SAJDN23135123");
+		p2.setPreco(2000.0);	
+		p2.setDescricao("Console");
+		p2.setStatus(true);
+		
+		listaDeProdutos.add(p2);
+		
+		model.addAttribute("listaDeProdutos", listaDeProdutos);
+		return "produtos";
 	}
 	@GetMapping("/novo-prod")
     public String novoProduto()	{
